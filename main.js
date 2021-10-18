346e28426b6c6075396fad43b44d0dac2da406b1
precious <preciousd533@gmail.com>
2021-10-17 23:02:26 -0700

    the java way

---
+++ main.js
@@ -0,0 +1,36 @@
+//console.dir(document)
+
+// examine the document object
+/*console.log(document.domain)
+console.log(document.URL)
+console.log(document.title)
+document.title =  " preciouszy";
+console.log(document.doctype)
+console.log(document.head)
+console.log(document.body)
+console.log(document.all)
+document.all[8].textContent = 'preciouszy'
+console.log(document.links)
+document.links[2].textContent = 'about';*/
+
+//console.log(document.getElementsByClassName('logo'))
+
+//var logo = document.getElementById('logo')
+//var container = document.getElementsByClassName('container')
+//console.log(logo)
+//logo.textContent = 'preciouszy'
+//logo.innerText = ' the man called z'
+//logo.innerHTML = ' <h4> preciouszy </h4>'
+
+//container[0].style.borderBottom = ' solid 3px
+
+// query selector
+//var container = document.querySelector('.container');
+//container.style.borderBottom = 'solid 4px #ccc';
+
+// query selector all
+
+//var container2 = document.querySelectorAll('.container2');
+
+//console.log(container2)
+//container2[0].textContent = 'hello';
